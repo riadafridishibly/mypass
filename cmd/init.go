3104b757bdeb643a1b20e689433bb8f7c488fbf2
Riad <riadafridishibly@gmail.com>
2023-04-15 03:44:49 +0600

    Refactor init, mainly support sqlite3 backend

--- cmd/init.go
+++ cmd/init.go
@@ -24,55 +24,54 @@ package cmd
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"os"
 	"path/filepath"
-	"strings"
 	"time"
 
 	"filippo.io/age"
+	"github.com/riadafridishibly/mypass/backend"
+	"github.com/riadafridishibly/mypass/config"
 	"github.com/riadafridishibly/mypass/models"
 	"github.com/riadafridishibly/mypass/vkeys"
 	"github.com/spf13/cobra"
-	jww "github.com/spf13/jwalterweatherman"
 	"github.com/spf13/viper"
 	"golang.org/x/term"
 )
 
-func exHome(p string) string {
-	v, err := expandedHome(p)
-	if err != nil {
-		jww.FATAL.Fatalf("Failed to get home dir: %v", err)
-	}
-	return v
-}
-
-func initPrivateKeysAndDb() {
-	// Create key pair
-	identity, err := age.GenerateX25519Identity()
-	if err != nil {
-		jww.ERROR.Fatalf("Failed to create X25519 Key pairs: %v", err)
+func initPrivateKeys(file string) (pubKeys []string, err error) {
+	// If err == nil; file exists
+	if _, err := os.Stat(file); err == nil {
+		return nil, errors.New("private key file exists")
 	}
 
 	fmt.Print("Enter your master password: ")
 	password, err := term.ReadPassword(int(os.Stdin.Fd()))
 	if err != nil {
-		jww.FATAL.Fatal("Failed to read password")
+		return nil, fmt.Errorf("failed to read master password: %w", err)
+		// jww.FATAL.Fatal("Failed to read password")
 	}
 	fmt.Println()
 	fmt.Print("Enter your master password (again): ")
 	password2, err := term.ReadPassword(int(os.Stdin.Fd()))
 	if err != nil {
-		jww.FATAL.Fatal("Failed to read password")
+		return nil, fmt.Errorf("failed to read master password: %w", err)
+		// jww.FATAL.Fatal("Failed to read password")
 	}
 	fmt.Println()
 
 	if !bytes.Equal(password, password2) {
-		jww.ERROR.Fatal("Password didn't match")
+		// jww.ERROR.Fatal("Password didn't match")
+		return nil, fmt.Errorf("password didn't match")
 	}
 
 	viper.Set(vkeys.Password, string(password))
 
+	identity, err := age.GenerateX25519Identity()
+	if err != nil {
+		return nil, fmt.Errorf("failed to create X25519 Key pairs: %v", err)
+	}
 	privKeys := models.PrivateKeys{
 		Meta: models.Meta{
 			CreatedAt: time.Now(),
@@ -82,41 +81,60 @@ func initPrivateKeysAndDb() {
 			models.SymSecretStr(identity.String()),
 		},
 	}
-
-	db := models.Database{
-		PublicKeys: []string{
-			identity.Recipient().String(),
-		},
-	}
-
-	err = os.MkdirAll(exHome("~/.mypass"), 0o0700)
+	// OpenFile(name, O_RDWR|O_CREATE|O_TRUNC, 0666)
+	f, err := os.OpenFile(file, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
 	if err != nil {
-		jww.FATAL.Fatalf("Failed to create dir: %v: err: %v", exHome("~/.mypass"), err)
+		return nil, err
 	}
+	defer f.Close()
 
-	jww.INFO.Printf("Writing database to: %s", viper.GetString(vkeys.DatabasePath))
-	err = writeJSON(viper.GetString(vkeys.DatabasePath), db)
+	err = json.NewEncoder(f).Encode(privKeys)
 	if err != nil {
-		jww.FATAL.Fatalf("Failed to write database: %v", err)
+		return nil, err
 	}
-	jww.INFO.Printf("Writing private keys to: %s", viper.GetString(vkeys.PrivateKeysPath))
-	err = writeJSON(viper.GetString(vkeys.PrivateKeysPath), privKeys)
-	if err != nil {
-		jww.FATAL.Fatalf("Failed to write private_keys: %v", err)
-	}
+	return []string{identity.Recipient().String()}, err
 }
 
-func fileExists(name string) bool {
-	_, err := os.Stat(name)
-	return err == nil
+var ErrConfigExits = errors.New("config already exits")
+
+func initDefaultConfig() error {
+	file := viper.ConfigFileUsed()
+	if file != "" {
+		return ErrConfigExits
+	}
+	data, err := config.DefaultConfig.ToYaml()
+	if err != nil {
+		return err
+	}
+	// Populate default config
+	viper.SetConfigType("yaml")
+	err = viper.MergeConfig(bytes.NewReader(data))
+	if err != nil {
+		return err
+	}
+	return os.WriteFile(DefaultConfigPath, data, 0644)
 }
 
-func isDBExists() bool {
-	return fileExists(viper.GetString(vkeys.DatabasePath))
-}
+func initPrivateKeysAndDb() error {
+	// Maybe define config root?
+	dir := filepath.Dir(viper.GetString(vkeys.PrivateKeysPath))
+	err := os.MkdirAll(dir, 0o0700)
+	if err != nil {
+		// jww.FATAL.Fatalf("Failed to create dir: %v: err: %v", exHome("~/.mypass"), err)
+		return err
+	}
 
-func isPrivKeysExists() bool {
-	return fileExists(viper.GetString(vkeys.PrivateKeysPath))
+	pubKeys, err := initPrivateKeys(viper.GetString(vkeys.PrivateKeysPath))
+	if err != nil {
+		return err
+	}
+
+	b, err := backend.Get()
+	if err != nil {
+		return err
+	}
+
+	return b.AddPublicKeys(pubKeys...)
 }
 
 // initCmd represents the init command
@@ -127,67 +145,20 @@ var initCmd = &cobra.Command{
 
 If the application is already initialized it'll do nothing.
 In case of error it'll report them in stdout.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		file := viper.ConfigFileUsed()
-		// FIXME: if config exists, but db and priv keys doesn't then create them
-		if file != "" {
-			jww.WARN.Printf("Config file exists: %v", file)
-			dbExists := isDBExists()
-			privKeyExists := isPrivKeysExists()
-			if dbExists {
-				jww.FATAL.Fatal("Database file already exists in:", viper.GetString(vkeys.DatabasePath))
-			}
-			if privKeyExists {
-				jww.FATAL.Fatal("Private keys file already exists in:", viper.GetString(vkeys.PrivateKeysPath))
-			}
-
-			jww.INFO.Print("Initializing private keys and database files...")
-			initPrivateKeysAndDb()
-			os.Exit(0)
+	SilenceUsage: true,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		// Try initialize default config!
+		err := initDefaultConfig()
+		if err != nil && !errors.Is(err, ErrConfigExits) {
+			return err
 		}
 
-		// Set config
-		viper.Set(vkeys.PrivateKeysPath, exHome("~/.mypass/private_keys"))
-		viper.Set(vkeys.DatabasePath, exHome("~/.mypass/db"))
-		initPrivateKeysAndDb()
-		vp := viper.New()
-		vp.Set(vkeys.DatabasePath, viper.GetString(vkeys.DatabasePath))
-		vp.Set(vkeys.PrivateKeysPath, viper.GetString(vkeys.PrivateKeysPath))
-
-		jww.INFO.Printf("Writing config file to: %s", "~/.mypass.yaml")
-		err := vp.WriteConfigAs(exHome("~/.mypass.yaml"))
+		err = initPrivateKeysAndDb()
 		if err != nil {
-			jww.ERROR.Fatalf("Failed to write config file: %v", err)
+			return err
 		}
+		return nil
 	},
-}
-
-func expandedHome(p string) (string, error) {
-	p = strings.TrimPrefix(p, "~/")
-	home, err := os.UserHomeDir()
-	if err != nil {
-		return "", err
-	}
-	return filepath.Join(home, p), nil
-}
-
-func writeJSON(filepath string, v any) error {
-	data, err := json.MarshalIndent(v, "", "  ")
-	if err != nil {
-		return err
-	}
-	f, err := os.OpenFile(filepath, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
-	if err != nil {
-		return err
-	}
-	defer f.Close()
-	_, err = f.Write(data)
-	if err != nil {
-		// TODO: handle failure case gracefully
-		fmt.Println(string(data))
-	}
-	_, err = f.Write([]byte("\n"))
-	return err
 }
 
 func init() {
